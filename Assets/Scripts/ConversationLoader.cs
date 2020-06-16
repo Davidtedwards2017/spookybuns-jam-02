653bddb87bc90071a694ab58504e6cca8554d7c1
DESKTOP-GCUT3GL\Tyler Edwards <tyler@tinyhorsegame.com>
2020-06-15 22:37:43 -0700

    added support for multiple conversation files

--- Assets/Scripts/ConversationLoader.cs
+++ Assets/Scripts/ConversationLoader.cs
@@ -7,20 +7,19 @@ using System.Linq;
 
 public class ConversationLoader : Singleton<ConversationLoader>
 {
-    public string FilePath
-    {
-        get
-        {
-            return Application.dataPath + "/StreamingAssets/" + "basic_conversation.json";
-        }
-    }
+    private const string FILE_PATTERN = "conversation_*.json";
 
     public List<DialogueData> ConversationData = new List<DialogueData>();
 
     // Start is called before the first frame update
     void Awake()
     {
-        ConversationData = Load(FilePath);
+        ConversationData = new List<DialogueData>();
+        var conversationFiles = Directory.GetFiles(Application.streamingAssetsPath, FILE_PATTERN);
+        foreach(var file in conversationFiles)
+        {
+            ConversationData.AddRange(Load(file));
+        }
     }
 
     public DialogueData GetCharacterConversation(GameInfo.Character character)
@@ -32,7 +31,7 @@ public class ConversationLoader : Singleton<ConversationLoader>
     {
         if (File.Exists(filePath))
         {
-            var text = File.ReadAllText(FilePath);
+            var text = File.ReadAllText(filePath);
             var jobj = JObject.Parse(text);
             return DataExtensions.NodesFromJObject(jobj);
         }
